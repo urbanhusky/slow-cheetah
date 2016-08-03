0b7c112e507be7cb05571cde534458b3fb02ec9b
urbanhusky <urbanhusky@users.noreply.github.com>
2016-08-03 11:52:37 +0200

    Updated NuGet; Created forked package; Use MSBuild 14

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
-"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" build.proj /p:Configuration=Release /p:VisualStudioVersion=12.0 /flp1:v=d;logfile=msbuild.d.log /flp2:v=diag;logfile=msbuild.diag.log
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" build.proj /p:Configuration=Release /p:VisualStudioVersion=14.0 /flp1:v=d;logfile=msbuild.d.log /flp2:v=diag;logfile=msbuild.diag.log
 
